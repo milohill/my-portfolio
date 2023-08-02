537fdc967586dc1fb5814d97d024cc298243addc
milohill <116620023+milohill@users.noreply.github.com>
2023-08-02 19:58:54 +0900

    Move the position of the about me title into the center of the page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,33 +6,36 @@ import AboutImage250 from '../assets/about-image-250.jpg';
 export default function About() {
   return (
     <div className="about">
-      <div className="about__image-container">
-        <img
-          className="about__image"
-          src={AboutImage250}
-          srcSet={`${AboutImage400} 400w,
+      <h1>About Me</h1>
+      <div className="about__container">
+        <div className="about__image-container">
+          <img
+            className="about__image"
+            src={AboutImage250}
+            srcSet={`${AboutImage400} 400w,
       ${AboutImage250} 250w`}
-          sizes="30vw"
-          alt="about image"
-        />
-        <div className="about__image about__image_background"></div>
-      </div>
-      <div className="about__text">
-        <h1>About Me</h1>
-        <h3>Milohill</h3>
-        <p>
-          I am a passionate and self-motivated full-stack developer, constantly
-          on a journey of learning and growth. My insatiable curiosity and love
-          for all things tech have driven me to explore the vast world of
-          software development, mastering both front-end and back-end
-          technologies.
-          <br />
-          <br />
-          From the very beginning of my self-learning journey, I have been
-          captivated by the art of coding. The ability to create something from
-          scratch, turning lines of code into functional and visually appealing
-          applications, gives me an unparalleled sense of fulfillment.
-        </p>
+            sizes="30vw"
+            alt="about image"
+          />
+          <div className="about__image about__image_background"></div>
+        </div>
+        <div className="about__text-container">
+          <h3>Milohill</h3>
+          <p>
+            I am a passionate and self-motivated full-stack developer,
+            constantly on a journey of learning and growth. My insatiable
+            curiosity and love for all things tech have driven me to explore the
+            vast world of software development, mastering both front-end and
+            back-end technologies.
+            <br />
+            <br />
+            From the very beginning of my self-learning journey, I have been
+            captivated by the art of coding. The ability to create something
+            from scratch, turning lines of code into functional and visually
+            appealing applications, gives me an unparalleled sense of
+            fulfillment.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,52 +9,56 @@ export default function Projects() {
   return (
     <div className="projects">
       <h1>Projects</h1>
-      <div className="projects__project-container">
-        <div className="projects__project-image-container">
-          <img
-            className="projects__project-image"
-            src={ProjectImageOne300}
-            alt="project image one"
-            srcSet={`${ProjectImageOne300} 300w, ${ProjectImageOne700} 700w`}
-            sizes="50vw"
-          />
-          <div className="projects__project-image projects__project-image_background"></div>
+      <div className="projects__container">
+        <div className="projects__project-container">
+          <div className="projects__project-image-container">
+            <img
+              className="projects__project-image"
+              src={ProjectImageOne300}
+              alt="project image one"
+              srcSet={`${ProjectImageOne300} 300w, ${ProjectImageOne700} 700w`}
+              sizes="50vw"
+            />
+            <div className="projects__project-image projects__project-image_background"></div>
+          </div>
+          <div className="projects__project-text">
+            <h3 className="projects__project-name">
+              Random Password Generator
+            </h3>
+            <p className="projects__project-description">
+              In this project, I developed a personal finance tracker app to
+              help users manage their finances effectively. The app allows users
+              to input income, expenses, and savings data, and then it generates
+              visualizations and reports to give users a comprehensive overview
+              of their financial health.
+            </p>
+          </div>
         </div>
-        <div className="projects__project-text">
-          <h3 className="projects__project-name">Random Password Generator</h3>
-          <p className="projects__project-description">
-            In this project, I developed a personal finance tracker app to help
-            users manage their finances effectively. The app allows users to
-            input income, expenses, and savings data, and then it generates
-            visualizations and reports to give users a comprehensive overview of
-            their financial health.
-          </p>
-        </div>
-      </div>
-      <div className="projects__project-container">
-        <div className="projects__project-text">
-          <h3 className="projects__project-name">
-            Immersive Rollercoaster Simulation
-          </h3>
-          <p className="projects__project-description">
-            Welcome to the world of adrenaline-pumping excitement and
-            breathtaking thrills!
-            <br />
-            The Immersive Rollercoaster Simulation project is an ambitious
-            endeavor that brings the ultimate rollercoaster experience to life,
-            transcending reality and immersing users in a virtual realm of
-            heart-pounding adventure.
-          </p>
-        </div>
-        <div className="projects__project-image-container">
-          <img
-            className="projects__project-image"
-            src={ProjectImageTwo300}
-            alt="project image one"
-            srcSet={`${ProjectImageTwo300} 300w, ${ProjectImageTwo700} 700w`}
-            sizes="50vw"
-          />
-          <div className="projects__project-image projects__project-image_background"></div>
+        <div className="projects__project-container">
+          <div className="projects__project-text">
+            <h3 className="projects__project-name">
+              Immersive Rollercoaster Simulation
+            </h3>
+            <p className="projects__project-description">
+              Welcome to the world of adrenaline-pumping excitement and
+              breathtaking thrills!
+              <br />
+              The Immersive Rollercoaster Simulation project is an ambitious
+              endeavor that brings the ultimate rollercoaster experience to
+              life, transcending reality and immersing users in a virtual realm
+              of heart-pounding adventure.
+            </p>
+          </div>
+          <div className="projects__project-image-container">
+            <img
+              className="projects__project-image"
+              src={ProjectImageTwo300}
+              alt="project image one"
+              srcSet={`${ProjectImageTwo300} 300w, ${ProjectImageTwo700} 700w`}
+              sizes="50vw"
+            />
+            <div className="projects__project-image projects__project-image_background"></div>
+          </div>
         </div>
       </div>
     </div>
